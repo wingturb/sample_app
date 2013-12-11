01d0cfd6d6ab6f1f29744967f307a8fe1f2b9168
xiaojun <xiaojun.wang1985@gmail.com>
2013-12-11 23:47:25 +0800

    till 5.1.2

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  before(:each) do
+    @base_title = "Sample App | "
+  end
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    it "should have the right title" do 
+      get 'home'
+      response.should have_selector("title", :content => @base_title + "Home")
+    end
+
   end
 
   describe "GET 'contact'" do
@@ -14,6 +22,11 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do 
+      get 'contact'
+      response.should have_selector("title", :content => @base_title + "Contact")
+    end
+
   end
 
   describe "GET 'about'" do
@@ -21,9 +34,20 @@ describe PagesController do
       get 'about'
         response.should be_success
     end
-    it describe "should have the right title" do 
+    it "should have the right title" do 
       get 'about'
-      response.should have_selector("title", :content => "Sample App | About")
+      response.should have_selector("title", :content => @base_title + "About")
     end
   end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'help'
+        response.should be_success
+    end
+    it describe "should have the right title" do 
+      get 'help'
+      response.should have_selector("title", :content => @base_title + "Help")
+    end
+  end 
 end
